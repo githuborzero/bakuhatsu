4be736aa3fe442f8b1da86f4e38e58b47f31e836
Joshua Lam <jlam26.ca@gmail.com>
2019-02-22 14:21:39 -0800

    secret.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import databaseSecrets from './../secret.js';
+// import databaseSecrets from './../secret.js';
 
 class App extends Component {
   render() {
